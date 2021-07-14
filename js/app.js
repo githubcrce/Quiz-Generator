1dee6901cc0d6c5d0e1229f4cd5cc03ca446357d
Warren Fernandes <75610517+warrenferns@users.noreply.github.com>
2021-07-14 10:55:25 +0530

    Created Quiz (#1)

---
+++ js/app.js
@@ -0,0 +1,154 @@
+const questionNumber = document.querySelector(".question-number");
+const questionText = document.querySelector(".question-text");
+const optionContainer = document.querySelector(".option-container");
+const answersIndicatorContainer = document.querySelector(".answers-indicator");
+const homeBox = document.querySelector(".home-box");
+const quizBox = document.querySelector(".quiz-box");
+const resultBox = document.querySelector(".result-box");
+const questionLimit = 5;
+
+let questionCounter = 0;
+let currentQuestion;
+let availableQuestions = [];
+let availableOptions = [];
+let correctAnswers = 0;
+let attempt = 0;
+
+function setAvailableQuestions(){
+    const totalQuestions = quiz.length;
+    for(let i=0; i<totalQuestions; i++){
+        availableQuestions.push(quiz[i])
+    }
+}
+
+function getNewQuestion(){
+    questionNumber.innerHTML = "Question " + (questionCounter+1) + " of " + questionLimit;
+    const questionIndex = availableQuestions[Math.floor(Math.random()*availableQuestions.length)];
+    currentQuestion = questionIndex;
+    questionText.innerHTML = currentQuestion.q;
+    const index1 = availableQuestions.indexOf(questionIndex);
+    availableQuestions.splice(index1,1);
+    if(currentQuestion.hasOwnProperty("img")){
+        const img = document.createElement("img");
+        img.src = currentQuestion.img;
+        questionText.appendChild(img);
+    }
+    const optionsLength = currentQuestion.options.length;
+    for(let i=0; i<optionsLength; i++){
+        availableOptions.push(i);
+    }
+    optionContainer.innerHTML = '';
+    let animationDelay = 0.2;
+    for(let i=0; i<optionsLength; i++){
+        const optionIndex = availableOptions[Math.floor(Math.random()*availableOptions.length)];
+        const index2 = availableOptions.indexOf(optionIndex);
+        availableOptions.splice(index2,1);
+        const option = document.createElement("div");
+        option.innerHTML = currentQuestion.options[optionIndex];
+        option.id = optionIndex;
+        option.style.animationDelay = animationDelay+'s';
+        animationDelay = animationDelay+0.15;
+        option.className = "option";
+        optionContainer.appendChild(option);
+        option.setAttribute("onclick", "getResult(this)");
+    }
+    questionCounter++;
+}
+
+function getResult(element){
+    const id = parseInt(element.id);
+    if(id === currentQuestion.answer){
+        element.classList.add("correct");
+        updateAnswerIndicator("correct");
+        correctAnswers++;
+    }
+    else{
+        element.classList.add("wrong");
+        updateAnswerIndicator("wrong");
+        const optionLength = optionContainer.children.length;
+        for(let i=0; i<optionLength; i++){
+            if(parseInt(optionContainer.children[i].id)===currentQuestion.answer) {
+                optionContainer.children[i].classList.add("correct");
+            }
+        }
+    }
+    attempt++;
+    unclickableOptions();
+}
+
+function unclickableOptions(){
+    const optionLength = optionContainer.children.length;
+    for(let i=0; i<optionLength; i++){
+        optionContainer.children[i].classList.add("already-answered");
+    }
+}
+
+function next(){
+    if(questionCounter === questionLimit){
+        quizOver();
+    }else{
+        getNewQuestion()
+    }
+}
+
+function answersIndicator(){
+    answersIndicatorContainer.innerHTML = '';
+    const totalQuestion = questionLimit;
+    for(let i=0; i<totalQuestion; i++){
+        const indicator = document.createElement("div");
+        answersIndicatorContainer.appendChild(indicator);
+
+    }
+}
+
+function updateAnswerIndicator(markType){
+    answersIndicatorContainer.children[questionCounter-1].classList.add(markType)
+}
+
+function quizOver(){
+    quizBox.classList.add("hide");
+    resultBox.classList.remove("hide");
+    quizResult();
+}
+
+function quizResult(){
+    resultBox.querySelector(".total-question").innerHTML = questionLimit;
+    resultBox.querySelector(".total-correct").innerHTML = correctAnswers;
+    resultBox.querySelector(".total-attempt").innerHTML = attempt;
+    resultBox.querySelector(".total-wrong").innerHTML = attempt - correctAnswers;
+    const percentage = (correctAnswers/questionLimit)*100;
+    resultBox.querySelector(".percentage").innerHTML = percentage.toFixed(2) + "%";
+    resultBox.querySelector(".total-score").innerHTML = correctAnswers + " / " + questionLimit;
+}
+
+function resetQuiz(){
+    questionCounter = 0;
+    correctAnswers = 0;
+    attempt = 0;
+    availableQuestions = [];
+}
+
+function tryAgain(){
+    resultBox.classList.add("hide");
+    quizBox.classList.remove("hide");
+    resetQuiz();
+    startQuiz();
+}
+
+function goToHome(){
+    resultBox.classList.add("hide");
+    homeBox.classList.remove("hide");
+    resetQuiz();
+}
+
+function startQuiz(){
+    homeBox.classList.add("hide");
+    quizBox.classList.remove("hide");
+    setAvailableQuestions();
+    getNewQuestion();
+    answersIndicator();
+}
+
+window.onload = function (){
+    homeBox.querySelector(".total-question").innerHTML = ""+questionLimit;
+}
